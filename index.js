e099d9bd8710316e16cc74c9b66bd8fd5f37886f
PromelHridoy <promel.hridoy12@gmail.com>
2026-06-20 08:16:26 +0600

    create study room POST API with MongoDB integration

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config();
 
@@ -8,7 +9,9 @@ const uri = process.env.MONGODB_URI;
 const app = express()
 
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5000;
+app.use(cors())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -21,21 +24,41 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const db = client.db("study-nook");
+    const studyNookCollection = db.collection("rooms");
+
+    app.post('/rooms', async (req, res) => {
+        const studyNookData = req.body;
+        console.log(studyNookData);
+        const result = await studyNookCollection.insertOne(studyNookData);
+        res.send(result);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir); 
 
 
 app.get('/', (req, res) =>  {
-    res.send("Server is running find")
+    res.send("Server is running fine")
 })
 
 app.listen(PORT, () => {
